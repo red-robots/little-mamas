28ba47b72598f116a829ac3064ebb6eb348c5b54
accrane <austincrane@Austins-MBP-2.attlocal.net>
2020-06-04 14:21:24 -0400

    fix popup

--- footer.php
+++ footer.php
@@ -75,13 +75,15 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php if(is_front_page()) { ?>
+<?php 
+$on = get_field('turn_on', 'option');
+if( $on[0] == 'yes' && is_front_page() ) : ?>
 <script type="text/javascript">
 	window.onload = function() {
 		$.colorbox({inline:true, href:".ajax"});
 	}
 </script>
-<?php } ?>
+<?php endif; ?>
 
 <?php wp_footer(); ?>
 
