4917aaff4bab490b253a83bc8314bbb9c0714224
accrane <austincrane@Austins-MBP-2.attlocal.net>
2020-06-08 10:40:11 -0400

    add analytics

--- header.php
+++ header.php
@@ -7,7 +7,11 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Lora:400,400i,700,700i&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-<?php wp_head(); ?>
+<?php
+wp_head();
+$analytics = get_field('analytics', 'option');
+if( $analytics ){echo $analytics;}
+?>
 </head>
 <?php $custom_class = (has_banner()) ? 'hasbanner':'nobanner'; ?>
 <body <?php body_class($custom_class); ?>>
@@ -33,7 +37,7 @@
 			<p><strong>This content comes from a hidden element on this page.</strong></p>
 			<p>The inline option preserves bound JavaScript events and changes, and it puts the content back where it came from when it is closed.</p>
 			<p><a id="click" href="#" style='padding:5px; background:#ccc;'>Click me, it will be preserved!</a></p>
-			
+
 			<p><strong>If you try to open a new Colorbox while it is already open, it will update itself with the new content.</strong></p>
 			<p>Updating Content Example:<br />
 			<a class="ajax" href="../content/ajax.html">Click here to load new content</a></p>
@@ -45,12 +49,12 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="wrapper">
 
-			<?php  
+			<?php
 			$reservation_title = get_field("reservation_title","option");
 			$reservation_link = get_field("reservation_link","option");
 			$order_options = get_field('order_options','option');
 			?>
-			
+
 			<?php if ($order_options || $reservation_link) { ?>
 			<div class="topmenu">
 				<div class="links">
@@ -60,20 +64,20 @@
 					<?php if ($order_options) { ?>
 						<a href="#" id="orderOption" class="orange">Order</a>
 						<div class="order-options">
-							<?php foreach ($order_options as $o) { 
+							<?php foreach ($order_options as $o) {
 								$o_link = $o['link'];
-								$o_logo = $o['logo'];  
+								$o_logo = $o['logo'];
 								$o_text = $o['text']; ?>
 								<?php if ($o_link && $o_logo) { ?>
 									<div class="orderlink">
 										<a href="<?php echo $o_link ?>" target="_blank">
 											<img src="<?php echo $o_logo['url'] ?>" alt="<?php echo $o['logo']['title'] ?>">
 											<?php if ($o_text) { ?>
-											<span class="text"><?php echo $o_text ?></span>	
+											<span class="text"><?php echo $o_text ?></span>
 											<?php } ?>
 										</a>
 									</div>
-								<?php } ?>	
+								<?php } ?>
 							<?php } ?>
 							<div id="closeOrder" class="closediv clear"><span id="close-order">Close</span></div>
 						</div>
@@ -81,7 +85,7 @@
 				</div>
 			</div>
 			<?php } ?>
-			
+
 			<div class="inner">
 				<?php if( get_custom_logo() ) { ?>
 		            <div class="logo">
