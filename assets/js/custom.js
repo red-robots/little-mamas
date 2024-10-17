aa174221ccbbb6366652d1c6776c6024456dbf48
accrane <austincrane@gmail.com>
2024-10-17 11:54:37 -0400

    add popup back - lost styles

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,24 +6,25 @@
  *	Developed by: Austin Crane	s
  *	Designed by: Austin Cranes
  */
+
 jQuery(document).ready(function ($) {
   var config = {
     "venueId": 10695,
     "apiKey": "4S3Om8mWKxIqSGqSABsH5A9s6u3ZTIPo"
   };
   $("#resyButton-9PTNB768ZJ").on("click", function () {
-    resyWidget.openModal(config); // console.log('asdfa');
-  }); // $("#resyButton-9PTNB768ZJ span").removeAttr("style");
+    resyWidget.openModal(config);
+    // console.log('asdfa');
+  });
+  // $("#resyButton-9PTNB768ZJ span").removeAttr("style");
   // $("#resyButton-9PTNB768ZJ span:hover").removeAttr("style");
   // $("#resyButton-9PTNB768ZJ span").addClass("resybutt");
 
   /* Slideshow */
-
   var swiper = new Swiper('.slideshow', {
     slidesPerView: 1,
     spaceBetween: 0,
     effect: 'fade',
-
     /* "fade", "cube", "coverflow" or "flip" */
     loop: true,
     autoplay: {
@@ -38,11 +39,10 @@ jQuery(document).ready(function ($) {
       prevEl: '.swiper-button-prev'
     }
   });
-  /* Order Option */
 
+  /* Order Option */
   $(document).on("click", "#orderOption", function (e) {
     e.preventDefault();
-
     if ($("div.order-options").length > 0) {
       $("div.order-options").slideToggle(300);
     }
@@ -51,30 +51,27 @@ jQuery(document).ready(function ($) {
     e.preventDefault();
     $("#orderOption").trigger('click');
   });
-  /* Resize Height of Top Noise Bakground */
 
+  /* Resize Height of Top Noise Bakground */
   resize_topbg();
   $(window).on("resize", function () {
     resize_topbg();
   });
-
   function resize_topbg() {
     var frameH = $("img.frame").height();
     var divHelper = $(".bghelper").height();
     var siteHead = $("#masthead").height();
     var totalHeight = divHelper + siteHead;
-
     if (totalHeight > siteHead) {
       $(".topbg").css("height", totalHeight + "px");
     }
   }
+
   /*
   *
   *	Wow Animation
   *
   ------------------------------------*/
-
-
   new WOW().init();
   $(document).on("click", "#toggleMenu", function () {
     $(this).toggleClass('open');
@@ -84,8 +81,7 @@ jQuery(document).ready(function ($) {
 }); // END #####################################    END
 "use strict";
 
-function _typeof2(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof2 = function _typeof2(obj) { return typeof obj; }; } else { _typeof2 = function _typeof2(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof2(obj); }
-
+function _typeof2(o) { "@babel/helpers - typeof"; return _typeof2 = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof2(o); }
 function _typeof(e) {
   return (_typeof = "function" == typeof Symbol && "symbol" == _typeof2(Symbol.iterator) ? function (e) {
     return _typeof2(e);
@@ -93,109 +89,93 @@ function _typeof(e) {
     return e && "function" == typeof Symbol && e.constructor === Symbol && e !== Symbol.prototype ? "symbol" : _typeof2(e);
   })(e);
 }
-
 var resyWidget = function () {
   var w,
-      e,
-      k,
-      C,
-      E,
-      I,
-      x = {},
-      L = {},
-      _ = "https://widgets.resy.com/",
-      A = function () {
-    var e = document.getElementsByTagName("script")[0],
+    e,
+    k,
+    C,
+    E,
+    I,
+    x = {},
+    L = {},
+    _ = "https://widgets.resy.com/",
+    A = function () {
+      var e = document.getElementsByTagName("script")[0],
         t = e.style.color;
-
-    try {
-      e.style.color = "rgba(1,5,13,0.44)";
-    } catch (e) {}
-
-    var n = e.style.color !== t;
-    return e.style.color = t, n;
-  }(),
-      g = "function" == typeof window.matchMedia && window.matchMedia("(-webkit-min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)").matches,
-      t = "default",
-      n = {
-    default: function _default(e) {
-      return (e || "").split("-").join("_");
-    },
-    java: function java(e) {
-      var t = (e || "").split("-").join("_"),
+      try {
+        e.style.color = "rgba(1,5,13,0.44)";
+      } catch (e) {}
+      var n = e.style.color !== t;
+      return e.style.color = t, n;
+    }(),
+    g = "function" == typeof window.matchMedia && window.matchMedia("(-webkit-min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)").matches,
+    t = "default",
+    n = {
+      default: function _default(e) {
+        return (e || "").split("-").join("_");
+      },
+      java: function java(e) {
+        var t = (e || "").split("-").join("_"),
           n = t.split("_");
-      return 1 < n.length ? "".concat(n[0].toLowerCase(), "_").concat(n[1].toUpperCase()) : t;
-    },
-    bcp47: function bcp47(e) {
-      var t = (e || "").split("_").join("-"),
+        return 1 < n.length ? "".concat(n[0].toLowerCase(), "_").concat(n[1].toUpperCase()) : t;
+      },
+      bcp47: function bcp47(e) {
+        var t = (e || "").split("_").join("-"),
           n = t.split("-");
-
-      switch (n.length) {
-        case 1:
-          n[0] = n[0].toLowerCase();
-          break;
-
-        case 2:
-          n[0] = n[0].toLowerCase(), 4 === n[1].length ? n[1] = n[1].charAt(0).toUpperCase() + n[1].slice(1).toLowerCase() : n[1] = n[1].toUpperCase();
-          break;
-
-        case 3:
-          n[0] = n[0].toLowerCase(), n[1] = n[1].charAt(0).toUpperCase() + n[1].slice(1).toLowerCase(), n[2] = n[2].toUpperCase();
-          break;
-
-        default:
-          return t;
+        switch (n.length) {
+          case 1:
+            n[0] = n[0].toLowerCase();
+            break;
+          case 2:
+            n[0] = n[0].toLowerCase(), 4 === n[1].length ? n[1] = n[1].charAt(0).toUpperCase() + n[1].slice(1).toLowerCase() : n[1] = n[1].toUpperCase();
+            break;
+          case 3:
+            n[0] = n[0].toLowerCase(), n[1] = n[1].charAt(0).toUpperCase() + n[1].slice(1).toLowerCase(), n[2] = n[2].toUpperCase();
+            break;
+          default:
+            return t;
+        }
+        return n.join("-");
+      },
+      "iso639-1": function iso6391(e) {
+        return (e || "").split("_").join("-").split("-")[0].toLowerCase();
       }
-
-      return n.join("-");
     },
-    "iso639-1": function iso6391(e) {
-      return (e || "").split("_").join("-").split("-")[0].toLowerCase();
-    }
-  },
-      h = {
-    ca: {
-      label: "Resy - Reservar",
-      url: "".concat(_, "images/resy-book-now-ca-9e14b24e92.svg")
+    h = {
+      ca: {
+        label: "Resy - Reservar",
+        url: "".concat(_, "images/resy-book-now-ca-9e14b24e92.svg")
+      },
+      en: {
+        label: "Resy - Book Now",
+        url: "".concat(_, "images/resy-book-now-en-d490d865ff.svg")
+      },
+      es: {
+        label: "Resy - Reservar",
+        url: "".concat(_, "images/resy-book-now-es-dc4622f113.svg")
+      },
+      fr: {
+        label: "Resy - Reserver",
+        url: "".concat(_, "images/resy-book-now-fr-408a64cdee.svg")
+      }
     },
-    en: {
-      label: "Resy - Book Now",
-      url: "".concat(_, "images/resy-book-now-en-d490d865ff.svg")
-    },
-    es: {
-      label: "Resy - Reservar",
-      url: "".concat(_, "images/resy-book-now-es-dc4622f113.svg")
-    },
-    fr: {
-      label: "Resy - Reserver",
-      url: "".concat(_, "images/resy-book-now-fr-408a64cdee.svg")
-    }
-  },
-      m = function () {
-    var e = function () {
-      var e,
+    m = function () {
+      var e = function () {
+        var e,
           t,
           n = window.navigator,
           o = ["language", "browserLanguage", "systemLanguage", "userLanguage"];
-      if (Array.isArray(n.languages)) for (e = 0; e < n.languages.length; e += 1) {
-        if ((t = n.languages[e]) && t.length) return t;
-      }
-
-      for (e = 0; e < o.length; e += 1) {
-        if ((t = n[o[e]]) && t.length) return t;
-      }
-
-      return null;
-    }() || "";
-
-    n[t] && (e = n[t](e));
-    return e;
-  }();
-
+        if (Array.isArray(n.languages)) for (e = 0; e < n.languages.length; e += 1) if ((t = n.languages[e]) && t.length) return t;
+        for (e = 0; e < o.length; e += 1) if ((t = n[o[e]]) && t.length) return t;
+        return null;
+      }() || "";
+      n[t] && (e = n[t](e));
+      return e;
+    }();
   function T(n, o) {
     var i,
-        r,
-        a = !1;
+      r,
+      a = !1;
     return "undefined" == typeof requestAnimationFrame ? n(1) : requestAnimationFrame(function e(t) {
       a || (void 0 === r && (i = (r = t) + o), t < i ? (n((t - r) / o), requestAnimationFrame(e)) : n(1));
     }), {
@@ -204,7 +184,6 @@ var resyWidget = function () {
       }
     };
   }
-
   function R(t) {
     try {
       return Object.keys(t).map(function (e) {
@@ -214,63 +193,48 @@ var resyWidget = function () {
       return "";
     }
   }
-
   function O(e, t) {
     var n,
-        o = {};
-
-    for (n in e) {
-      void 0 !== e[n] && (o[n] = e[n]);
-    }
-
-    for (n in t) {
-      void 0 !== t[n] && (o[n] = t[n]);
-    }
-
+      o = {};
+    for (n in e) void 0 !== e[n] && (o[n] = e[n]);
+    for (n in t) void 0 !== t[n] && (o[n] = t[n]);
     return o;
   }
-
   function S() {
     I && I.focus && (I.focus(), I.tagName && "input" === I.tagName.toLowerCase() && "function" == typeof I.select && I.select()), "object" === _typeof(k) && void 0 !== k.removeChild && (document.body.removeChild(k), k = void 0), "string" == typeof x.overflow && (document.body.style.overflow = x.overflow), "string" == typeof x.position && (document.body.style.position = x.position), "string" == typeof x.height && (document.body.style.minHeight = x.height), "string" == typeof x.htmlHeight && (document.documentElement.style.height = x.htmlHeight), x = {}, "function" == typeof E && E();
   }
-
   function v(e, t, n) {
     var o,
-        i,
-        r,
-        a,
-        s,
-        c,
-        l,
-        d,
-        u,
-        y = !1;
-
+      i,
+      r,
+      a,
+      s,
+      c,
+      l,
+      d,
+      u,
+      y = !1;
     function f() {
       y || (y = !0, "undefined" == typeof requestAnimationFrame ? window.setTimeout(function () {
         p();
       }, 30) : requestAnimationFrame(p));
     }
-
     function p() {
       var e = document.body.clientWidth - 20;
       700 < e ? e = 700 : e < 300 && (e = 300), (document.body.clientWidth - e) % 2 != 0 && (e -= 1), w.style.width = "".concat(e, "px"), y = !1;
     }
-
     function g(e) {
       return {
         innerHeight: e.innerHeight,
         innerWidth: e.innerWidth
       };
     }
-
     I = n, S();
-
     try {
       i = O(L, e || {});
       var h = g(window),
-          m = h.innerWidth,
-          v = h.innerHeight;
+        m = h.innerWidth,
+        v = h.innerHeight;
       if (o = {
         ref: window.location.href,
         src: i.src || "".concat(window.location.hostname, "-widget"),
@@ -278,7 +242,6 @@ var resyWidget = function () {
         innerWidth: m
       }, "function" == typeof i.onClose) E = i.onClose, delete i.onClose;
       if ("string" == typeof i.apiKey && (o.apiKey = i.apiKey), "string" == typeof i.stylesheet && (o.stylesheet = i.stylesheet), "string" == typeof i.affId && (o.affId = i.affId), "string" == typeof i.offerId && (o.offerId = i.offerId), "number" != typeof i.venueId && parseInt(i.venueId).toString() !== i.venueId) throw new Error("ResyWidget: Venue Id was not set");
-
       if (o.venueId = i.venueId, "string" == typeof i.firstName && (o.firstName = i.firstName), "string" == typeof i.lastName && (o.lastName = i.lastName), "string" == typeof i.email && (o.email = i.email), "string" == typeof i.phone && (o.phone = i.phone), "string" == typeof i.airbnbBookingId && (o.airbnbBookingId = i.airbnbBookingId), o.firstName && o.lastName && o.email && o.phone && (o.signup = !0), "resy.com" === location.host || "blog.resy.com" === location.host) {
         for (s in l = function () {
           for (var e = {}, t = window.location.search.substring(1).split("&"), n = 0; n < t.length; n += 1) {
@@ -288,15 +251,10 @@ var resyWidget = function () {
               e[o[0]] = i;
             } else e[o[0]].push(o[1]);
           }
-
           return e;
-        }(), r = /^utm_/, l) {
-          s.match(r) && (o[s] = decodeURIComponent(l[s]));
-        }
-
+        }(), r = /^utm_/, l) s.match(r) && (o[s] = decodeURIComponent(l[s]));
         "string" == typeof l.gclid && (o.gclid = l.gclid);
       }
-
       d = _, i.hasOwnProperty("authToken") && "string" == typeof i.authToken && (o.authToken = i.authToken);
       var b = "string" == typeof i.widgetPath;
       if (!0 !== i.reservation || b ? i.hasOwnProperty("resyToken") && "string" == typeof i.resyToken && !b ? d += "?".concat(R(o), "#/account/reservations/").concat(i.resyToken) : i.hasOwnProperty("conciergeResyToken") && "string" == typeof i.conciergeResyToken && !b ? d += "?".concat(R(o), "#/account/reservations/").concat(i.conciergeResyToken, "/1") : (b ? d += "?".concat(R(o), "#/").concat(i.widgetPath) : i.addOnsTemplateId ? d += "?".concat(R(o), "#/venues/").concat(i.venueId, "/add-ons/").concat(i.addOnsTemplateId) : i.isPickups ? d += "?".concat(R(o), "#/venues/").concat(i.venueId, "/pickups") : d += "?".concat(R(o), "#/venues/").concat(i.venueId), ("string" == typeof i.date || "string" == typeof i.seats || "number" == typeof i.seats || b) && (a = {}, t && "object" === _typeof(t) && (a.reservation = JSON.stringify(t)), "string" == typeof i.resyToken && (a.resyToken = i.resyToken), "string" == typeof i.date && (a.date = i.date), void 0 !== i.seats && (a.seats = i.seats), void 0 !== i.maxPartySize && (a.maxPartySize = i.maxPartySize), void 0 !== i.tableConfigId && (a.tableConfigId = i.tableConfigId), void 0 !== i.timeslot && (a.timeslot = i.timeslot), void 0 !== i.notify_request_id && (a.notify_request_id = i.notify_request_id), void 0 !== i.request_start && (a.request_start = i.request_start), void 0 !== i.request_end && (a.request_end = i.request_end), void 0 !== i.notifyService && (a.notifyService = i.notifyService), void 0 !== i.addOnsTemplateId && (a.addOnsTemplateId = i.addOnsTemplateId), d += "?".concat(R(a)))) : d += "?".concat(R(o), "#/account/reservations/"), /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) return void window.open(d, "_blank");
@@ -313,7 +271,6 @@ var resyWidget = function () {
       console.error(e.message);
     }
   }
-
   return m = -1 !== m.indexOf("ca") ? "ca" : -1 !== m.indexOf("en") ? "en" : -1 !== m.indexOf("es") ? "es" : -1 !== m.indexOf("fr") ? "fr" : "en", window.addEventListener && window.addEventListener("message", function (n) {
     if ("resyHeightChanged" === n.data.event) {
       var o = parseFloat(w.style.height);
@@ -325,18 +282,12 @@ var resyWidget = function () {
   }), {
     addButton: function addButton(e, t) {
       var n, o, i, r, a;
-
       if ("object" === _typeof(e) && null !== e) {
         var s, c, l;
-
         if (a = !0 === (o = O(L, t = t || {})).replace || "true" === o.replace, Array.isArray && Array.isArray(e) || (c = (s = e).length, "undefined" !== (l = _typeof(s)) && null !== l && "function" !== l && s !== s.window && (1 === s.nodeType && c || "array" === l || 0 === c || "number" == typeof c && 0 < c && c - 1 in s)) || (e = [e]), a) {
-          for (r = [], n = 0; n < e.length; n += 1) {
-            r.push(e[n]);
-          }
-
+          for (r = [], n = 0; n < e.length; n += 1) r.push(e[n]);
           e = r;
         }
-
         for (n = 0; n < e.length; n += 1) {
           (i = document.createElement("span")).style.background = "#1d4223", i.style.borderRadius = "0px", i.style.cursor = "pointer", i.style.display = "inline-block", i.style.height = "50px", i.style.position = "relative", i.style.width = "500px", i.style.float = "left", i.setAttribute("role", "button"), i.setAttribute("aria-label", h[t.locale ? t.locale : m].label);
           var d = document.createElement("iframe");
@@ -345,15 +296,12 @@ var resyWidget = function () {
           u.style.height = "50px", u.style.position = "absolute", u.style.left = "0px", u.style.top = "0px", u.style.width = "200px", u.style.zIndex = 1, i.appendChild(u), i.addEventListener ? (i.addEventListener("click", p(o)), i.addEventListener("mouseenter", y), i.addEventListener("mouseleave", f)) : i.attachEvent && (i.attachEvent("click", p(o)), i.attachEvent("mouseenter", y), i.attachEvent("mouseleave", f)), a ? e[n].parentNode.replaceChild(i, e[n]) : e[n].appendChild(i);
         }
       }
-
       function y() {
         i.style.background = "#e72415";
       }
-
       function f() {
         i.style.background = "#ff462d";
       }
-
       function p(e) {
         return function () {
           v(e);
--- inc/scripts.php
+++ inc/scripts.php
@@ -7,7 +7,7 @@ function bellaworks_scripts() {
     'bellaworks-style',
      get_stylesheet_uri(),
      array(),
-     '1.12'
+     '1.13'
   );
 
   wp_deregister_script('jquery');
